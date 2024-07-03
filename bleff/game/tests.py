82399e2877c29df327ab8cff625e4a6282fa46ec
marcosdima <marcosdimatteo.dev@gmail.com>
2024-07-03 16:14:29 -0300

    new: more test for word model

--- bleff/game/tests.py
+++ bleff/game/tests.py
@@ -1,3 +1,4 @@
+from django.forms import ValidationError
 from django.test import TestCase
 
 from .models import Word, Language, Meaning
@@ -9,5 +10,20 @@ class WordModelTest(TestCase):
         '''
         text = 'House'
         word = Word.objects.create(word=text)
-        self.assertIs(word.word, text)
-        
+
+        try:
+            word.full_clean()
+        except ValidationError:
+            self.fail("Valid word raised ValidationError")
+
+    
+    def test_create_a_word_with_blank_as_value(self):
+        word = Word(word='')
+        with self.assertRaises(ValidationError):
+            word.full_clean()
+
+
+    def test_create_a_word_with_no_values(self):
+        word = Word()
+        with self.assertRaises(ValidationError):
+            word.full_clean()
