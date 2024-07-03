e572e1fc89e33c762336b4216365d658b1f74bc0
marcosdima <marcosdimatteo.dev@gmail.com>
2024-07-03 16:15:56 -0300

    mod: adding validators

--- bleff/game/models.py
+++ bleff/game/models.py
@@ -2,13 +2,14 @@ from django.core.validators import MinLengthValidator
 from django.db import models
 
 class Word(models.Model):
-    word = models.CharField(max_length=40, unique=True)
+    word = models.CharField(max_length=40, unique=True, validators=[MinLengthValidator(3)])
 
     def __str__(self):
         return self.word
 
+
 class Language(models.Model):
-    tag = models.CharField(max_length=3, primary_key=True)
+    tag = models.CharField(max_length=3, primary_key=True, validators=[MinLengthValidator(1)])
     name = models.CharField(max_length=20, unique=True)
 
 
