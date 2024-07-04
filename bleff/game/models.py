735051f69a8ed21f50aa791c743ac370869de8c9
marcosdima <marcosdimatteo.dev@gmail.com>
2024-07-04 13:16:10 -0300

    new: str functions

--- bleff/game/models.py
+++ bleff/game/models.py
@@ -12,9 +12,15 @@ class Language(models.Model):
     tag = models.CharField(max_length=3, primary_key=True, validators=[MinLengthValidator(1)])
     name = models.CharField(max_length=20, unique=True, validators=[MinLengthValidator(4)])
 
+    def __str__(self):
+        return f'{self.tag}-{self.name}'
+
 
 class Meaning(models.Model):
     text = models.CharField(max_length=200)
     word_translation = models.CharField(max_length=40)
     word = models.ForeignKey(Word, on_delete=models.CASCADE)
     language = models.ForeignKey(Language, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.word_translation}: {self.text}'
