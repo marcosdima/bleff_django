43cab8d80469c86c54a207686973675657c79652
marcosdima <marcosdimatteo.dev@gmail.com>
2024-07-10 11:04:04 -0300

    new: signal to create the right answer when a Hand is created

--- bleff/game/signals.py
+++ bleff/game/signals.py
@@ -1,9 +1,14 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
-from .models import Game, Play
+from .models import Game, Play, Hand, Guess, Meaning
 
 @receiver(post_save, sender=Game)
 def creator_play_creation(sender, instance, created, **kwargs):
     if created and instance.creator:
         Play.objects.create(game=instance, user=instance.creator)
+
+@receiver(post_save, sender=Hand)
+def creator_right_guess(sender, instance, created, **kwargs):
+    if created:
+        Guess.objects.create(content=Meaning.objects.filter(word=instance.word)[0].text, is_original=True, hand=instance)
