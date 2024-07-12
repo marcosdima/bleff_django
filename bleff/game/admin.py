cdf8f81c8ab2757d89b4ac2149d06b32582d17aa
marcosdima <marcosdimatteo.dev@gmail.com>
2024-07-12 09:53:17 -0300

    new: admin configuration

--- bleff/game/admin.py
+++ bleff/game/admin.py
@@ -1,8 +1,22 @@
 from django.contrib import admin
 
-from .models import Game, Word, Meaning, Language
+from .models import Game, Word, Meaning, Language, Play
 
+class PlayInLine(admin.TabularInline):
+    model = Play
+    extra = 2
 
-@admin.register(Game, Word, Meaning, Language)
+class MeaningInLine(admin.TabularInline):
+    model = Meaning
+
+@admin.register(Game)
+class GameAdmin(admin.ModelAdmin):
+    inlines = [PlayInLine]
+
+@admin.register(Word)
+class WordAdmin(admin.ModelAdmin):
+    inlines = [MeaningInLine]
+
+@admin.register(Language)
 class PersonAdmin(admin.ModelAdmin):
     pass
