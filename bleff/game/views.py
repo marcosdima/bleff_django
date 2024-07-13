2463a8013b00e73d16db782fec826e0fa43a11fb
marcosdima <marcosdimatteo.dev@gmail.com>
2024-07-13 09:34:10 -0300

    new: waiting view

--- bleff/game/views.py
+++ bleff/game/views.py
@@ -1,7 +1,19 @@
 from django.shortcuts import render
 from django.views import generic
-from .models import Game
+from django.contrib.auth.models import User
+
+from .models import Game, Play
 
 class IndexView(generic.ListView):
     model=Game
     template_name = "game/index.html"
+
+
+class WaitingView(generic.ListView):
+    model=User
+    template_name = "game/waiting.html"
+
+    def get_queryset(self): 
+        id = self.kwargs.get('game_id')
+        return [p.user for p in Play.objects.filter(game=id)]
+ 
