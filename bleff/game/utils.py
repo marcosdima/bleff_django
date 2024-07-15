e7de63754062043a0903ac15651ac43bf3edb902
marcosdima <marcosdimatteo.dev@gmail.com>
2024-07-15 10:37:03 -0300

    new: utils file

---
+++ bleff/game/utils.py
@@ -0,0 +1,29 @@
+import random
+
+from .models import Hand, Play, Game, Word, Meaning
+
+def plays_game(user, game_id) -> bool:
+    return Play.objects.filter(game=game_id, user=user).exists()
+
+
+def is_leader(user, game_id) -> bool:
+    return Hand.objects.get(game=game_id, finished_at=None).leader.id == user.id
+
+
+def get_game_words_choice(game_id, n_words) -> list:
+    game = Game.objects.get(id=game_id)
+    result = []
+    possible_words = [m.word.id for m in Meaning.objects.filter(language=game.idiom)]
+    played_words = [h.word.id for h in Hand.objects.filter(game=game)]
+
+    while len(possible_words) > 0 and len(result) < n_words:
+        word = random.choice(possible_words)
+
+        if not word in played_words:
+            result.append(Word.objects.get(id=word))
+
+        possible_words.remove(word)
+
+    return result
+
+
