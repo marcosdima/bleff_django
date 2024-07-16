13d60e45c2bb1c09ebe8b9626f48b4a6c9dd660b
marcosdima <marcosdimatteo.dev@gmail.com>
2024-07-16 20:12:19 -0300

    removed: now this function is not needed anymore

--- bleff/game/utils.py
+++ bleff/game/utils.py
@@ -14,20 +14,3 @@ def get_game_hand(game_id):
     # Check if exists a hand
     if Hand.objects.all().exists():
         return Hand.objects.get(game=game_id, finished_at=None)
-
-
-def get_game_words_choice(game_id, n_words) -> list:
-    game = Game.objects.get(id=game_id)
-    result = []
-    possible_words = [m.word.id for m in Meaning.objects.filter(language=game.idiom).exclude(word=None)]
-    played_words = [h.word.id for h in Hand.objects.filter(game=game).exclude(word=None)]
-
-    while len(possible_words) > 0 and len(result) < n_words:
-        word = random.choice(possible_words)
-
-        if not word in played_words:
-            result.append(Word.objects.get(id=word))
-
-        possible_words.remove(word)
-
-    return result
