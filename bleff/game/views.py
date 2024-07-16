f61591973a1845a12bb6145eea1a5eac8d979b17
marcosdima <marcosdimatteo.dev@gmail.com>
2024-07-16 10:21:20 -0300

    new: enter game view

--- bleff/game/views.py
+++ bleff/game/views.py
@@ -1,9 +1,13 @@
+from django.db import IntegrityError
 from django.shortcuts import redirect
 from django.views import generic
 from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.decorators.http import require_POST
 
 from .models import Game, Play, Hand
+from .utils import plays_game
 
 class IndexView(generic.ListView):
     model=Game
@@ -30,18 +34,37 @@ class WaitingView(LoginRequiredMixin, generic.ListView):
         return [p.user for p in Play.objects.filter(game=id)]
     
 
+@login_required
+@require_POST
+def enter_game(request):
+    game_id = request.POST['game']
+
+    try:
+        game = Game.objects.get(id=game_id)
+        Play.objects.create(game=game, user=request.user)
+        return redirect('game:waiting', game_id=game.id)
+    except IntegrityError as e:
+        if plays_game(user=request.user, game_id=game_id):
+            return redirect('game:waiting', game_id=game.id)
+    except Exception as e:
+        # TODO: message error and validation of 'user is already playin another game'.
+        print(e)
+
+    return redirect('game:index')
+
+
 def start_game(request, game_id):
     game = Game.objects.get(pk=game_id)
     
+    alreadyEnded = game.finished_at != None
     alreadyStarted = Hand.objects.filter(game=game).exists()
 
-    if alreadyStarted:
+    if alreadyEnded:
+        return redirect('game:index')
+    elif alreadyStarted:
         # TODO: In this case it should send the user to the actual hand.
         pass
-    elif not game.creator:
-        # TODO: If the game has no creator recorded, then it should start.
-        pass
-    elif game.creator.id == request.user.id:
+    elif not game.creator or game.creator.id == request.user.id:
         # TODO: The creator says 'START THE GAME!', so it should start.
         pass
 
