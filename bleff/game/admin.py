7bb5c084e12004402322ac7eaad1e86717a310d5
marcosdima <marcosdimatteo.dev@gmail.com>
2024-07-17 20:36:11 -0300

    new: GuessInLine and HandAdmin

--- bleff/game/admin.py
+++ bleff/game/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Game, Word, Meaning, Language, Play, Hand
+from .models import Game, Word, Meaning, Language, Play, Hand, Guess, Vote
 
 class PlayInLine(admin.TabularInline):
     model = Play
@@ -16,6 +16,10 @@ class HandInLine(admin.TabularInline):
     extra = 0
 
 
+class GuessInLine(admin.TabularInline):
+    model = Guess
+    extra = 2
+
 @admin.register(Game)
 class GameAdmin(admin.ModelAdmin):
     inlines = [PlayInLine, HandInLine]
@@ -26,6 +30,13 @@ class WordAdmin(admin.ModelAdmin):
     inlines = [MeaningInLine]
 
 
+@admin.register(Hand)
+class HandAdmin(admin.ModelAdmin):
+    inlines = [GuessInLine]
+
+
 @admin.register(Language)
 class PersonAdmin(admin.ModelAdmin):
     pass
+
+
