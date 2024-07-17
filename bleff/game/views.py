31d331ab7de89c7888e7f2e91ed6ac507716adef
marcosdima <marcosdimatteo.dev@gmail.com>
2024-07-17 13:16:34 -0300

    new: handle_redirection function, creation and implementation

--- bleff/game/views.py
+++ bleff/game/views.py
@@ -9,7 +9,23 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.decorators.http import require_POST
 
 from .models import Game, Play, Hand
-from .utils import plays_game, get_game_hand
+from .utils import plays_game, get_game_hand, get_hand_choice_words
+
+def handle_redirection(request):
+    # If does not exists a Play with this user and a game unfinished.
+    is_playing_something = Play.objects.filter(user=request.user).exclude(game__finished_at__isnull=False)
+    if not is_playing_something:
+        return redirect('game:index')
+
+    # If exists a game, but does not start yet.
+    game = is_playing_something[0].game
+    game_start = Hand.objects.filter(game=game).exists()
+    if not game_start:
+        return redirect('game:waiting', game_id=game.id)
+    
+    # If exists a game and it started, then send the user to hand.
+    return HttpResponseRedirect(reverse("game:hand", args=(game.id,)))
+
 
 class IndexView(generic.ListView):
     model=Game
@@ -38,42 +54,45 @@ class WaitingView(LoginRequiredMixin, generic.ListView):
 @require_POST
 def enter_game(request):
     game_id = request.POST['game']
+    game = Game.objects.get(id=game_id)
 
+    if plays_game(user=request.user, game_id=game_id):
+        return handle_redirection(request=request)
+    
     try:
-        game = Game.objects.get(id=game_id)
         Play.objects.create(game=game, user=request.user)
-        return redirect('game:waiting', game_id=game.id)
-    except IntegrityError as e:
-        if plays_game(user=request.user, game_id=game_id):
-            return redirect('game:waiting', game_id=game.id)
     except Exception as e:
         # TODO: message error and validation of 'user is already playin another game'.
         print(e)
+        return handle_redirection(request=request)
 
-    return redirect('game:index')
+    return redirect('game:waiting', game_id=game.id)
 
 
 @login_required
 @require_POST
 def start_game(request, game_id):
     game = Game.objects.get(pk=game_id)
-    
+
     alreadyEnded = game.finished_at != None
     alreadyStarted = Hand.objects.filter(game=game).exists()
 
     if alreadyEnded or not plays_game(user=request.user, game_id=game_id):
         # If the game already ended or user does not play 'game_id' game, redirects to index.
-        return redirect('game:index')
-    elif alreadyStarted:
-        return HttpResponseRedirect(reverse("game:hand", args=(game.id,)))
-    elif not game.creator or game.creator.id == request.user.id:
+        return handle_redirection(request=request)
+    
+    if not alreadyStarted and (not game.creator or game.creator.id == request.user.id):
         Hand.objects.create(game=game)
-        return HttpResponseRedirect(reverse("game:hand", args=(game.id,)))
         
-    return redirect('game:waiting', game_id=game_id)
+    return handle_redirection(request=request)
 
 
 @login_required
 def hand_view(request, game_id):
     hand = get_game_hand(game_id)
-    return render(request, 'game/hand.html', {"hand": hand})
+    words = []
+
+    if request.user.id == hand.leader.id:
+        words = get_hand_choice_words(hand=hand)
+
+    return render(request, 'game/hand.html', {"hand": hand, "words_to_choose": words})
