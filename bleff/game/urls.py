87031d52ec1c504a2b63230c0bc75c93d3b0f9d8
marcosdima <marcosdimatteo.dev@gmail.com>
2024-07-17 20:05:28 -0300

    new: make_guess and guesses views urls

--- bleff/game/urls.py
+++ bleff/game/urls.py
@@ -11,4 +11,6 @@ urlpatterns = [
     path("start/<int:game_id>/", views.start_game, name="start_game"),
     path("<int:game_id>/hand/", views.hand_view, name="hand"),
     path("<int:game_id>/hand/choose/", views.choose_word, name="choose"),
+    path("<int:game_id>/hand/guess/", views.make_guess, name="make_guess"),
+    path("<int:game_id>/hand/guesses/", views.GuessesView.as_view(), name="guesses"),
 ]
