33ba76654da82f222551cb6fbcfd65730ef43f6d
marcosdima <marcosdimatteo.dev@gmail.com>
2024-07-18 19:16:16 -0300

    new: conditions_are_met function

--- bleff/game/utils.py
+++ bleff/game/utils.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.models import User
 from django.db.models import Model
 
-from .models import Hand, Play, Choice
+from .models import Hand, Play, Choice, Game, Condition, ConditionTag
 
 class FilteredObject:
     def __init__(self, dictionary: dict) -> None:
@@ -14,6 +14,12 @@ class FilteredObject:
         for k, v in self.__dict__.items():
             result += f'- {k}: {v} \n'
         return result
+
+
+class ConditionsResult:
+    def __init__(self, label: str, value_required: int) -> None:
+        self.label = label
+        self.value_required = value_required
 
 
 def plays_game(user: User, game_id: int) -> bool:
@@ -38,3 +44,21 @@ def remove_fields(object: Model, fields: list[str], filters=dict[str, any]) -> l
     objects_values = object.objects.filter(**filters).values()
 
     return [FilteredObject(dictionary={ field: value for field, value in object_value.items() if not field in fields }) for object_value in objects_values]
+
+
+def conditions_are_met(game_id: int) -> list[ConditionsResult]:
+    game = Game.objects.get(id=game_id)
+    conditions = Condition.objects.filter(game=game)
+    cant_players = Play.objects.filter(game=game).count()
+    result = []
+    
+    for condition in conditions:
+        tag = condition.tag.tag
+        value = condition.value
+
+        if tag == 'MAX_PLAYERS' and cant_players > value:
+            result.append(ConditionsResult(tag, value))
+        elif tag == 'MIN_PLAYERS' and cant_players < value:
+            result.append(ConditionsResult(tag, value))
+
+    return result
