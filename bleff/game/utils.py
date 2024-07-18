773c52fbeb2cd60d0cd62c9580ab19d2c71866cf
marcosdima <marcosdimatteo.dev@gmail.com>
2024-07-18 11:38:08 -0300

    new: remove_fields function new: FilteredObject to return an object with the filtered fields at remove_fields

--- bleff/game/utils.py
+++ bleff/game/utils.py
@@ -1,6 +1,20 @@
 from django.contrib.auth.models import User
+from django.db.models import Model
 
 from .models import Hand, Play, Choice
+
+class FilteredObject:
+    def __init__(self, dictionary: dict) -> None:
+        for k, v in dictionary.items():
+            setattr(self, k, v)
+
+    
+    def __str__(self) -> str:
+        result = f'Fields: \n'
+        for k, v in self.__dict__.items():
+            result += f'- {k}: {v} \n'
+        return result
+
 
 def plays_game(user: User, game_id: int) -> bool:
     return Play.objects.filter(game=game_id, user=user).exists()
@@ -15,5 +29,12 @@ def get_game_hand(game_id: int):
     if Hand.objects.all().exists():
         return Hand.objects.get(game=game_id, finished_at=None)
 
+
 def get_hand_choice_words(hand: Hand) -> list:
     return [c.word for c in Choice.objects.filter(hand=hand)]
+
+
+def remove_fields(object: Model, fields: list[str], filters=dict[str, any]) -> list[FilteredObject]:
+    objects_values = object.objects.filter(**filters).values()
+
+    return [FilteredObject(dictionary={ field: value for field, value in object_value.items() if not field in fields }) for object_value in objects_values]
