906d847237f5825ee319a3cff3eef3ca22caf820
marcosdima <marcosdimatteo.dev@gmail.com>
2024-07-18 20:42:52 -0300

    new: create url

--- bleff/game/urls.py
+++ bleff/game/urls.py
@@ -7,6 +7,7 @@ app_name = "game"
 urlpatterns = [
     path("", views.IndexView.as_view(), name="index"),
     path("enter/", views.enter_game, name="enter_game"),
+    path("create/", views.create_game, name="create"),
     path("<int:game_id>/", views.WaitingView.as_view(), name="waiting"),
     path("start/<int:game_id>/", views.start_game, name="start_game"),
     path("<int:game_id>/hand/", views.hand_view, name="hand"),
