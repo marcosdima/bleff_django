490c1577355dd31a3d1ce32b4bac39482a729e10
marcosdima <marcosdimatteo.dev@gmail.com>
2024-07-18 12:25:03 -0300

    new: decorator to check if request.user is hand leader

--- bleff/game/decorators.py
+++ bleff/game/decorators.py
@@ -2,7 +2,7 @@ from functools import wraps
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 
-from .utils import plays_game
+from .utils import plays_game, get_game_hand
 
 def play_required(handler):
     def decorator(view_func):
@@ -11,6 +11,21 @@ def play_required(handler):
             game_id = kwargs.get('game_id')
 
             if not plays_game(user=request.user, game_id=game_id):
+                return handler(request)
+            
+            return view_func(request, *args, **kwargs)
+        
+        return _wrapped_view
+    return decorator
+
+
+def leader_required(handler):
+    def decorator(view_func):
+        @wraps(view_func)
+        def _wrapped_view(request, *args, **kwargs):
+            game_id = kwargs.get('game_id')
+
+            if not get_game_hand(game_id=game_id).leader.id == request.user.id:
                 return handler(request)
             
             return view_func(request, *args, **kwargs)
