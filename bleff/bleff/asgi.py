6bc61a6be29e5dd2bc3dda42c6d9f0ea064b1866
marcosdima <marcosdimatteo.dev@gmail.com>
2024-08-17 12:45:34 -0300

    mod: asgi configuration

--- bleff/bleff/asgi.py
+++ bleff/bleff/asgi.py
@@ -1,13 +1,29 @@
 import os
 
+
 from channels.routing import ProtocolTypeRouter
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.security.websocket import AllowedHostsOriginValidator
+from django.core.asgi import get_asgi_application
 from django.core.asgi import get_asgi_application
 
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
+# Initialize Django ASGI application early to ensure the AppRegistry
+# is populated before importing code that may import ORM models.
+django_asgi_app = get_asgi_application()
+
+from game.routing import websocket_urlpatterns
+
 
 application = ProtocolTypeRouter(
     {
         "http": get_asgi_application(),
-        # Just HTTP for now.
+        "http": django_asgi_app,
+        "websocket": AllowedHostsOriginValidator(
+            AuthMiddlewareStack(URLRouter(websocket_urlpatterns))
+        ),
     }
 )
