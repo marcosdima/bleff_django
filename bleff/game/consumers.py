84393cb083e004a609bf38126c6fd79f0e78a176
marcosdima <marcosdimatteo.dev@gmail.com>
2024-08-20 19:49:00 -0300

    mod: consumer adapt

--- bleff/game/consumers.py
+++ bleff/game/consumers.py
@@ -2,6 +2,7 @@
 import json
 from asgiref.sync import async_to_sync
 from channels.generic.websocket import WebsocketConsumer
+from django.urls import reverse
 
 class GameConsumer(WebsocketConsumer):
     def connect(self):
@@ -27,6 +28,12 @@ class GameConsumer(WebsocketConsumer):
 
         if event_type == 'player_join':
             self.handle_player_join(text_data_json)
+        elif event_type == 'chosen_word':
+            self.handle_chosen_word()
+        elif event_type == 'start_game':
+            self.handle_start_game()
+        elif event_type == 'guesses_ready':
+            self.handle_guesses_ready()
 
 
     def handle_player_join(self, data):
@@ -41,7 +48,52 @@ class GameConsumer(WebsocketConsumer):
         )
 
 
+    def handle_chosen_word(self):
+        async_to_sync(self.channel_layer.group_send)(
+            self.game_name, 
+            {
+                "type": "chosen_word",
+            }
+        )
+
+
+    def handle_start_game(self):
+        async_to_sync(self.channel_layer.group_send)(
+            self.game_name, 
+            {
+                "type": "start_game",
+            }
+        )
+
+
+    def handle_guesses_ready(self):
+        async_to_sync(self.channel_layer.group_send)(
+            self.game_name, 
+            {
+                "type": "guesses_ready",
+            }
+        )
+
+
+    def start_game(self, event):
+        game_id = int(self.game_id)
+        self.send(text_data=json.dumps({"url": reverse('game:hand', args=(game_id,))}))
+
+
     def player_join(self, event):
         player_username = event['player_username']
 
         self.send(text_data=json.dumps({"player_username": player_username}))
+
+
+    def chosen_word(self, event):
+        self.send(text_data=json.dumps({"chosen_word": True}))
+
+
+    def new_guess(self, event):
+        new_guess = event['new_guess']
+        self.send(text_data=json.dumps({"new_guess": new_guess}))
+
+
+    def guesses_ready(self, event):
+        self.send(text_data=json.dumps({"guesses_ready": True}))
