4e72f2a5090d9effe1f984989b548029c6b142e1
marcosdima <marcosdimatteo.dev@gmail.com>
2024-08-20 15:21:46 -0300

    rework: replace individual js files for two files: WebSocketManager and WebSocketSetup. The first one holds an homonimus class that should handle the conection and basic websocket functionallity . The second one handle each template particular features.

---
+++ bleff/game/static/game/js/WebSocketManager.js
@@ -0,0 +1,54 @@
+class WebSocketManager {
+    constructor() {
+        if (!WebSocketManager.instance) {
+            this.socket = null;
+            this.handlers = {};
+            WebSocketManager.instance = this;
+        }
+        return WebSocketManager.instance;
+    }
+
+    connect(gameId) {
+        if (!this.socket || this.socket.readyState === WebSocket.CLOSED) {
+            this.socket = new WebSocket(`ws://${window.location.host}/ws/game/${gameId}/`);
+
+            this.socket.onopen = () => {
+                if (USERNAME) this.socket.send(JSON.stringify({
+                    'player_username': USERNAME,
+                    'event_type': 'player_join'
+                }));
+                console.log('WebSocket connection established');
+            };
+
+            this.socket.onmessage = (event) => {
+                this.handleMessage(event);
+            };
+
+            this.socket.onclose = () => {
+                console.log('WebSocket connection closed');
+            };
+        }
+    }
+
+    handleMessage(event) {
+        const data = JSON.parse(event.data);
+        const [container] = document.getElementsByClassName('container');
+        const template = container.getAttribute('data-template');
+
+        if (this.handlers[template]) {
+            this.handlers[template](data);
+        }
+    }
+
+    registerHandler(template, handler) {
+        this.handlers[template] = handler;
+    }
+
+    send(event) {
+        this.socket.send(event);
+    }
+}
+
+const instance = new WebSocketManager();
+
+export default instance;
---
+++ bleff/game/static/game/js/WebSocketSetup.js
@@ -0,0 +1,37 @@
+import WebSocketManager from './WebSocketManager.js';
+
+const wsManager = WebSocketManager;
+wsManager.connect(gameId);
+
+const [container] = document.getElementsByClassName('container');
+const currentTemplate = container.getAttribute('data-template');
+
+if (currentTemplate === 'waiting') {
+    wsManager.registerHandler('waiting', (data) => {
+        if (data.url) return window.location.href = data.url;
+
+        const newUsername = data.player_username;
+        const usernames = document.getElementById('usernames');
+
+        // If the username is the same as the current user or already was added...
+        if (USERNAME === newUsername || document.getElementById(newUsername)) return;
+
+        const newItem = document.createElement('li');
+        const h1Item = document.createElement('h1');
+
+        h1Item.textContent = newUsername;
+        newItem.id = newUsername;
+
+        newItem.appendChild(h1Item);
+        usernames.appendChild(newItem);
+    });
+
+    const button = document.querySelector('#start_game');
+    if (button) button.onclick = function(e) {
+        wsManager.send(JSON.stringify({
+            'event_type': 'start_game'
+        }));
+    };
+} else if (currentTemplate === 'hand') {
+    console.log('hand')
+}
