42f17bc67c80d1254f630013b31ed73ac22680c7
marcosdima <marcosdimatteo.dev@gmail.com>
2024-08-19 21:28:18 -0300

    mod: change some things

--- bleff/game/static/game/js/ws.js
+++ bleff/game/static/game/js/ws.js
@@ -1,19 +1,23 @@
-export function connectWebSocket(gameId, onMessageCallback = (data) => {}, onOpenCallback = () => {}) {
-    const socket = new WebSocket(`ws://${window.location.host}/ws/game/${gameId}/`);
+export default function connectWebSocket(
+    gameId,
+    onMessageCallback = (data) => {},
+    onOpenCallback = () => {}
+    ) {
+        const socket = new WebSocket(`ws://${window.location.host}/ws/game/${gameId}/`);
 
-    socket.onopen = function() {
-        onOpenCallback(socket)
-        console.log(`Connected to WebSocket: Game ${gameId}`);
-    };
+        socket.onopen = function() {
+            onOpenCallback(socket)
+            console.log(`Connected to WebSocket: Game ${gameId}`);
+        };
 
-    socket.onmessage = function(e) {
-        const data = JSON.parse(e.data);
-        onMessageCallback(data);
-    };
+        socket.onmessage = function(e) {
+            const data = JSON.parse(e.data);
+            onMessageCallback(data);
+        };
 
-    socket.onclose = function(e) {
-        console.error('WebSocket closed unexpectedly');
-    };
+        socket.onclose = function(e) {
+            console.error('WebSocket closed unexpectedly');
+        };
 
-    return socket;
+        return socket;
 }
